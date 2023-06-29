5a2b779dc4776b311444a5341f8698fdb4e21cf2
Csmally <1130180692@qq.com>
2023-06-29 11:09:47 +0800

    添加覆盖层，为toast和modal类组件做准备

--- src/pages/toast/index.tsx
+++ src/pages/toast/index.tsx
@@ -1,26 +1,46 @@
 import { getViewSize } from "@/utils/sizeTool"
+import { useEffect, useRef } from "react"
 import { StyleSheet } from "react-native"
 import { View, Text } from "react-native-ui-lib"
+import { Navigation } from 'react-native-navigation'
 
 const Toast: React.FC = () => {
+    let timerRef = useRef<any>()
+    useEffect(() => {
+        timerRef.current = setTimeout(() => {
+            Navigation.dismissAllOverlays()
+        }, 2500);
+        return () => {
+            clearTimeout(timerRef.current)
+        }
+    }, [])
     return (
-        // <View flex center>
-        // </View>
         <View style={styles.container}>
-            <Text color={'#FFFFFF'} center>我是toast我是toast我是toast我是toast我是toast</Text>
+            <View style={styles.content}>
+                <Text color={'#FFFFFF'} center>我是toast我是toast我是toast我是toast我是toast</Text>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        pointerEvents: 'none',
+
+        elevation: 5, // 设置阴影的程度-安卓
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+    },
+    content: {
         maxWidth: getViewSize(150),
-        backgroundColor: 'red',
+        backgroundColor: '#000000',
         padding: getViewSize(10),
         borderRadius: getViewSize(8),
-        top: '46%',
-        alignSelf: 'center',
-        
     }
 })
 
