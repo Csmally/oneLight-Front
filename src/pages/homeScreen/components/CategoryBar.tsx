3dc4722817c05e27b8f4ddbbcbaf24fa98a226a9
yangxuan09 <yangxuan09@100.me>
2023-09-22 17:25:01 +0800

    feat: 分类页点击切换tab完美解决方案

--- src/pages/homeScreen/components/CategoryBar.tsx
+++ src/pages/homeScreen/components/CategoryBar.tsx
@@ -1,9 +1,6 @@
-import { commonStyles } from "@/common/styles";
-import { getViewSize } from "@/utils/sizeTool";
 import { useState } from "react";
-import { StyleSheet, TouchableOpacity, Platform } from "react-native";
-import FastImage from "react-native-fast-image";
-import Animated, { AnimatedRef, Extrapolation, SharedValue, interpolate, interpolateColor, useAnimatedStyle } from "react-native-reanimated";
+import Animated, { AnimatedRef, SharedValue } from "react-native-reanimated";
+import CategoryItem from "./CategoryItem";
 
 const tabs = [
     {
@@ -34,91 +31,7 @@ type CategoryBarProps = {
 }
 
 function CategoryBar({ scrollY }: CategoryBarProps) {
-    console.log('9898刷新');
-    const [currentTab, setCurrentTab] = useState(0);
-    const changeTab = (index: number) => {
-        setCurrentTab(index);
-    };
-    // 分类背景色变化动画(选中)
-    const categoryColorAnimatedStyle = useAnimatedStyle(() => {
-        // 背景颜色
-        const backgroundColor = interpolateColor(scrollY.value, [0, 200], ['rgba(0, 0, 0,1)', 'rgba(0, 0, 0,0)']);
-        // 边距
-        const gapStyle = interpolate(scrollY.value, [0, 200], [10, 0], {
-            extrapolateLeft: Extrapolation.CLAMP,
-            extrapolateRight: Extrapolation.CLAMP,
-        });
-        // 圆角
-        const borderRadius = interpolate(scrollY.value, [0, 200], [25, 0], {
-            extrapolateLeft: Extrapolation.CLAMP,
-            extrapolateRight: Extrapolation.CLAMP,
-        });
-        return {
-            marginLeft: gapStyle,
-            marginVertical: gapStyle,
-            borderRadius,
-            backgroundColor,
-        };
-    });
-    // 映射分类栏阴影动画样式
-    const categoryBarShadowAnimatedStyle = useAnimatedStyle(() => {
-        // 阴影宽度 & 安卓elevation
-        const shadowWidth = interpolate(scrollY.value, [0, 200], [10, 0], {
-            extrapolateLeft: Extrapolation.CLAMP,
-            extrapolateRight: Extrapolation.CLAMP,
-        });
-        // 透明度
-        const shadowOpacity = interpolate(scrollY.value, [0, 200], [0.3, 0], {
-            extrapolateLeft: Extrapolation.CLAMP,
-            extrapolateRight: Extrapolation.CLAMP,
-        });
-        // 圆角
-        const shadowRadius = interpolate(scrollY.value, [0, 200], [2, 0], {
-            extrapolateLeft: Extrapolation.CLAMP,
-            extrapolateRight: Extrapolation.CLAMP,
-        });
-        // 背景颜色
-        const backgroundColor = interpolateColor(scrollY.value, [0, 200], ['rgba(255, 255, 255,1)', 'rgba(255, 255, 255,0)']);
-        const gapStyle = interpolate(scrollY.value, [0, 200], [10, 0], {
-            extrapolateLeft: Extrapolation.CLAMP,
-            extrapolateRight: Extrapolation.CLAMP,
-        });
-        // 圆角
-        const borderRadius = interpolate(scrollY.value, [0, 200], [25, 0], {
-            extrapolateLeft: Extrapolation.CLAMP,
-            extrapolateRight: Extrapolation.CLAMP,
-        });
-        return {
-            marginLeft: gapStyle,
-            marginVertical: gapStyle,
-            borderRadius,
-            shadowOffset: {
-                width: 0,
-                height: shadowWidth
-            },
-            shadowOpacity,
-            shadowRadius,
-            elevation: shadowWidth,
-            shadowColor: Platform.OS === 'ios' ? '#cecece' : '#000000',
-            backgroundColor
-        };
-    });
-    // 分类栏字体色变化动画(选中)
-    const categoryTextSColorSAnimatedStyle = useAnimatedStyle(() => {
-        // 背景颜色
-        const color = interpolateColor(scrollY.value, [0, 200], ['#ffffff', '#000000']);
-        return {
-            color
-        };
-    });
-    // 分类栏字体色变化动画(未选中)
-    const categoryTextNColorSAnimatedStyle = useAnimatedStyle(() => {
-        // 背景颜色
-        const color = interpolateColor(scrollY.value, [0, 200], ['#000000', '#ffffff']);
-        return {
-            color
-        };
-    });
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
     return (
         <Animated.ScrollView
             horizontal
@@ -127,55 +40,18 @@ function CategoryBar({ scrollY }: CategoryBarProps) {
         >
             {
                 tabs.map((item, index) => (
-                    <TouchableOpacity
+                    <CategoryItem
                         key={index}
-                        activeOpacity={0.6}
-                        onPress={() => changeTab(index)}
-                    >
-                        <Animated.View
-                            style={[
-                                styles.tabItem,
-                                index === currentTab ? categoryColorAnimatedStyle : categoryBarShadowAnimatedStyle
-                            ]}
-                        >
-                            <FastImage source={item.img} style={styles.tabIcon} />
-                            <Animated.Text
-                                style={[
-                                    styles.tabTitle,
-                                    index === currentTab ? categoryTextSColorSAnimatedStyle : categoryTextNColorSAnimatedStyle
-                                ]}
-                                ellipsizeMode='middle'
-                                numberOfLines={1}
-                            >
-                                {item.title}
-                            </Animated.Text>
-                        </Animated.View>
-                    </TouchableOpacity>
+                        categoryInfo={item}
+                        scrollY={scrollY}
+                        setActiveTabIndex={setActiveTabIndex}
+                        selfIndex={index}
+                        activeTabIndex={activeTabIndex}
+                    />
                 ))
             }
         </Animated.ScrollView >
     );
 }
-
-const styles = StyleSheet.create({
-    tabItem: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: getViewSize(100),
-        height: 50,
-        backgroundColor: commonStyles.white,
-        paddingHorizontal: getViewSize(20)
-    },
-    tabIcon: {
-        width: getViewSize(18),
-        height: getViewSize(18),
-        marginRight: getViewSize(5)
-    },
-    tabTitle: {
-        fontWeight: 'bold',
-        fontFamily: 'System'
-    },
-});
 
 export default CategoryBar;
---
+++ src/pages/homeScreen/components/CategoryItem.tsx
@@ -0,0 +1,127 @@
+import { commonStyles } from "@/common/styles";
+import { getViewSize } from "@/utils/sizeTool";
+import { Platform, StyleSheet, TouchableOpacity } from "react-native";
+import FastImage from "react-native-fast-image";
+import Animated, { Extrapolation, SharedValue, interpolate, interpolateColor, useAnimatedStyle } from "react-native-reanimated";
+
+type CategoryItemInfo = {
+    title: string,
+    img: any
+}
+type CategoryItemProps = {
+    scrollY: SharedValue<number>,
+    categoryInfo: CategoryItemInfo,
+    setActiveTabIndex: (index: number) => void,
+    selfIndex: number,
+    activeTabIndex: number
+}
+
+function CategoryItem({ categoryInfo, scrollY, setActiveTabIndex, selfIndex, activeTabIndex }: CategoryItemProps) {
+    // 分类栏item的容器动画
+    const categoryContainerAnimatedStyle = useAnimatedStyle(() => {
+        // 背景颜色
+        const backgroundColor = interpolateColor(scrollY.value, [0, 200], [
+            selfIndex === activeTabIndex ? 'rgba(0, 0, 0,1)' : 'rgba(255, 255, 255,1)',
+            selfIndex === activeTabIndex ? 'rgba(0, 0, 0,0)' : 'rgba(255, 255, 255,0)',
+        ]);
+        // 边距
+        const gapStyle = interpolate(scrollY.value, [0, 200], [10, 0], {
+            extrapolateLeft: Extrapolation.CLAMP,
+            extrapolateRight: Extrapolation.CLAMP,
+        });
+        // 圆角
+        const borderRadius = interpolate(scrollY.value, [0, 200], [25, 0], {
+            extrapolateLeft: Extrapolation.CLAMP,
+            extrapolateRight: Extrapolation.CLAMP,
+        });
+        // 阴影宽度 & 安卓elevation
+        const shadowWidth = interpolate(scrollY.value, [0, 200], [10, 0], {
+            extrapolateLeft: Extrapolation.CLAMP,
+            extrapolateRight: Extrapolation.CLAMP,
+        });
+        // 透明度
+        const shadowOpacity = interpolate(scrollY.value, [0, 200], [0.3, 0], {
+            extrapolateLeft: Extrapolation.CLAMP,
+            extrapolateRight: Extrapolation.CLAMP,
+        });
+        // 圆角
+        const shadowRadius = interpolate(scrollY.value, [0, 200], [2, 0], {
+            extrapolateLeft: Extrapolation.CLAMP,
+            extrapolateRight: Extrapolation.CLAMP,
+        });
+        const shadowStyle = selfIndex !== activeTabIndex ? {
+            shadowOffset: {
+                width: 0,
+                height: shadowWidth
+            },
+            shadowOpacity,
+            shadowRadius,
+            elevation: shadowWidth,
+            shadowColor: Platform.OS === 'ios' ? '#cecece' : '#000000',
+        } : {};
+        return {
+            marginLeft: gapStyle,
+            marginVertical: gapStyle,
+            borderRadius,
+            backgroundColor,
+            ...shadowStyle
+        };
+    });
+    // 分类栏字体色变化动画(选中)
+    const categoryTextColorAnimatedStyle = useAnimatedStyle(() => {
+        const color = interpolateColor(scrollY.value, [0, 200], [
+            selfIndex === activeTabIndex ? '#ffffff' : '#000000',
+            selfIndex === activeTabIndex ? '#000000' : '#ffffff',
+        ]);
+        return {
+            color
+        };
+    });
+    return (
+        <TouchableOpacity
+            activeOpacity={0.6}
+            onPress={() => setActiveTabIndex(selfIndex)}
+        >
+            <Animated.View
+                style={[
+                    styles.tabItem,
+                    categoryContainerAnimatedStyle
+                ]}
+            >
+                <FastImage source={categoryInfo.img} style={styles.tabIcon} />
+                <Animated.Text
+                    style={[
+                        styles.tabTitle,
+                        categoryTextColorAnimatedStyle
+                    ]}
+                    ellipsizeMode='middle'
+                    numberOfLines={1}
+                >
+                    {categoryInfo.title}
+                </Animated.Text>
+            </Animated.View>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    tabItem: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: getViewSize(100),
+        height: 50,
+        backgroundColor: commonStyles.white,
+        paddingHorizontal: getViewSize(20)
+    },
+    tabIcon: {
+        width: getViewSize(18),
+        height: getViewSize(18),
+        marginRight: getViewSize(5)
+    },
+    tabTitle: {
+        fontWeight: 'bold',
+        fontFamily: 'System'
+    },
+});
+export default CategoryItem;
